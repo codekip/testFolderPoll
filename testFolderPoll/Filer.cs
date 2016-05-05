886f70aacd04edbfc4f8549ac14d715985bcfa5d
nick <nick@NICK-MAC-WIN>
2016-05-05 21:57:58 +0100

    Add project files.

---
+++ testFolderPoll/Filer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace testFolderPoll
+{
+    public static class Filer
+    {
+        public static IEnumerable<string> GetFiles(string cUsersNickDesktopNewFolder)
+        {
+            var thefiles = Directory.GetFiles(cUsersNickDesktopNewFolder).ToList();
+
+            var subdir = Directory.GetDirectories(cUsersNickDesktopNewFolder);
+
+            foreach (var s in subdir)
+            {
+                var subdirfiles = ProcessDir(s);
+
+                thefiles.AddRange(subdirfiles);
+            }
+            return thefiles;
+
+        }
+
+        private static IEnumerable<string> ProcessDir(string dir)
+        {
+            var thefiles = Directory.GetFiles(dir).ToList();
+
+            return thefiles;
+        }
+    }
+}
---
+++ testFolderPoll/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace testFolderPoll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch ss = new Stopwatch();
+            ss.Start();
+            var x = Filer.GetFiles(@"C:\Users\nick");//C:\Users\nick\Desktop\New folder\
+            foreach (var f in x)
+            {
+                Console.WriteLine(f);
+            }
+
+            ss.Stop();
+            Console.WriteLine(ss.Elapsed.TotalSeconds + " seconds for " + x.Count() + " files");
+            Console.ReadLine();
+        }
+    }
+}
